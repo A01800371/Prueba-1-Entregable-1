8809d4203f046a5a0da66c915440be9303c76fa4
yael <yaelmichel.gl@gmail.com>
2025-04-13 00:44:52 -0600

    Escena del pueblo
    codigo para cambios de escena con colliders, tilemap del pueblo

---
+++ Prueba Entregable/Assets/Scripts/MovimientoSimple.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovimientoSimple : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f; // Desactiva la gravedad
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(moveX, moveY) * speed;
+        rb.linearVelocity = move;
+    }
+}
