4b78538aaf17e4caf455a702a70263b9664bd650
BrianAxel70 <A01753036@tec.mx>
2025-04-30 10:52:09 -0600

    Mas Traducciones

--- Prueba Entregable/Assets/Scripts/LevelData.cs
+++ Prueba Entregable/Assets/Scripts/LevelData.cs
@@ -3,13 +3,14 @@
 */
 
 using UnityEngine;
+using UnityEngine.Localization;
 
 [System.Serializable]
 
 public class LevelData
 {
     public string levelName;
-    public string description;
+    [SerializeField] public LocalizedString description;
     public Sprite previewImage;
     public string sceneName;
     public string introSceneName;
